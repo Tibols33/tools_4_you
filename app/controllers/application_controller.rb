67705328b80f2d9bb1a7000a38930a823bcab1f7
MargotRx <margot.roux.mr@gmail.com>
2023-11-22 14:49:08 +0100

    changed path homepage - login - dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    dashboard_path(current_user) # <- Path you want to redirect the user to.
+  end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path(current_user) # <- Path you want to redirect the user to.
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
